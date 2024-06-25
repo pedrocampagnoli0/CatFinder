d24ec7593d0be74284d2409e21437d8c37d8a747
Pedro Campagnoli <95457636+pedrocampagnoli0@users.noreply.github.com>
2024-06-25 20:08:09 -0300

    Add files via upload

---
+++ CatFinder/CatFinder/CatFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatFinderApp.swift
+//  CatFinder
+//
+//  Created by Turma02-20 on 25/06/24.
+//
+
+import SwiftUI
+
+@main
+struct CatFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
