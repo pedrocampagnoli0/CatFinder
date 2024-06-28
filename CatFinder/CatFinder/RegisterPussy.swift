1e48f1619ad5305cdf4cbc49312c31043f5687b9
Lucas Salatiel <110201578+lsalatiel@users.noreply.github.com>
2024-06-28 19:48:43 -0300

    Add files via upload

--- CatFinder/CatFinder/RegisterPussy.swift
+++ CatFinder/CatFinder/RegisterPussy.swift
@@ -20,6 +20,9 @@ struct RegisterPussy: View {
     @State var showImagePicker: Bool = false
     @State var image: Image? = nil;
     @State var imageArray: [IdentifiableImage] = [];
+    @State private var currentIndex = 0;
+    @State private var imagesCount = 0;
+    let timer = Timer.publish(every: .infinity, on: .main, in: .common).autoconnect()
     @State private var catName: String = "";
     var body: some View {
         ZStack {
@@ -42,23 +45,49 @@ struct RegisterPussy: View {
                 Spacer()
                 
                 VStack {
+                    Text("Insert Images: " + String(imagesCount) + "/18")
+                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                        .foregroundStyle(Color("text-color"))
                     Button(action: {
                         self.showImagePicker.toggle()
                     }) {
-                        Text("+")
+                        ZStack {
+                            Image("logo")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 300)
+                                .grayscale(0.8)
+                            
+                            Text("+")
+                                .font(.system(size: 130))
+                                .opacity(0.7)
+                                .foregroundStyle(.white)
+                        }
                     }
-                    .frame(width: 250, height: 300)
+                    .frame(width: 300, height: 300)
                     .background(Color("button-color"))
                     .foregroundColor(Color("background-color"))
                     .font(.largeTitle)
                     .bold()
-                    .cornerRadius(6)
+                    .cornerRadius(150)
                     
-                    HStack {
-                        ForEach(imageArray) { img in
-                            img.image
-                                .resizable()
-                                .frame(width: 50, height: 50)
+                    VStack {
+                        TabView(selection:$currentIndex) {
+                            ForEach(0..<imageArray.count, id: \.self) { imageIndex in
+                                imageArray[imageIndex].image
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 250, height: 160)
+                                    .cornerRadius(30)
+                                    .clipped()
+                            }
+                        }
+                        .tabViewStyle(PageTabViewStyle())
+                        .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+                    }
+                    .onReceive(timer) { _ in
+                        withAnimation {
+                            currentIndex = (currentIndex + 1) % imageArray.count
                         }
                     }
                 }
@@ -67,8 +96,10 @@ struct RegisterPussy: View {
                         self.image = Image(uiImage: image)
                         let img = IdentifiableImage(image: Image(uiImage: image))
                         self.imageArray.append(img)
+                        imagesCount += 1
                     }
                 }
+                .padding(.top)
                 
                 Spacer()
                 
